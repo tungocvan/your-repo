a3f96897e9c75bd67823a55f590c57a8d2a9741b
tungocvan <tungocvan@gmail.com>
2019-08-13 10:14:04 +0700

    Updates

---
+++ precache-manifest.1f7d2d6a84dd6e9b0f7b14ef064696a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aadad16cf40d781209eb3444939b1a3",
+    "url": "/your-repo/index.html"
+  },
+  {
+    "revision": "fb0ee8839026aacddcdd",
+    "url": "/your-repo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b51fa9f06d1c6b9d7e56",
+    "url": "/your-repo/static/js/2.21ed56be.chunk.js"
+  },
+  {
+    "revision": "fb0ee8839026aacddcdd",
+    "url": "/your-repo/static/js/main.b86e1cf9.chunk.js"
+  },
+  {
+    "revision": "426b9866e0dc86f680f0",
+    "url": "/your-repo/static/js/runtime~main.a6eb7c70.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/your-repo/static/media/logo.5d5d9eef.svg"
+  }
+]);
